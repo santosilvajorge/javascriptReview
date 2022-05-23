c8be175eb4c398567a5811d6e7ace5a3e97bb95c
santosilvajorge <santosilvajorge@gmail.com>
2022-05-23 22:27:27 +0100

    2

--- javascriptReview.js
+++ javascriptReview.js
@@ -1,4 +1,4 @@
-/* //1. Create a function to calculate the sum of the two given integers. If the two values are the same, return triple their sum.
+/* // 1. Create a function to calculate the sum of the two given integers. If the two values are the same, return triple their sum.
 let a = 9;
 let b = 9;
 
@@ -11,3 +11,18 @@ function SumIt () {
 }
 console.log(SumIt()) */
 
+
+
+/* // 2. Create a function to check two given integers. Return true if one of them is 50 or if their sum is 50.
+let a = 50;
+let b = 10;
+
+if (a + b == 50 || a == 50 || b == 50) {
+   console.log(true)
+} else {
+    console.log(false)
+} */
+
+
+
+// 
