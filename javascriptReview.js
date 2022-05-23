cae9c550cb6b99b875fa37adf0c6e6f98576c187
santosilvajorge <santosilvajorge@gmail.com>
2022-05-23 21:49:44 +0100

    update

---
+++ javascriptReview.js
@@ -0,0 +1,11 @@
+//1. Create a function to calculate the sum of the two given integers. If the two values are the same, return triple their sum.
+let a = 9;
+let b = 9;
+
+function SumIt () {
+    if (a == b) {
+        return a*3
+    } else {
+        return a + b 
+    }
+}
